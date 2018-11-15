b053085c16b0c75c605e2ea7bad63b50a8ef6e68
abhilash97sharma <45075999+abhilash97sharma@users.noreply.github.com>
2018-11-15 22:53:09 +0530

    Delete Hive_Context.py

--- Hive_Context.py
+++
@@ -1,6 +0,0 @@
-from pyspark import HiveContext
-from pyspark import SparkContext
-sc= SparkContext("local","Hive_Context")
-hv= HiveContext(sc)
-print("Objects of hive context and sparkContext created")
-hv.sql("select * from usrdb.emp")
