f83698543aa2b0990fc3da0b6eeddbfed799cb70
abhilash97sharma <45075999+abhilash97sharma@users.noreply.github.com>
2019-05-23 22:40:32 +0530

    Add files via upload

--- RDD_Basics.java
+++ RDD_Basics.java
@@ -1,25 +1,27 @@
-package com.Spark;
+package org.spark;
 
-import java.util.Arrays;
-
-import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
-
+import java.util.Arrays;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
 import scala.Tuple2;
 
-import org.apache.spark.api.java.JavaRDD;
-
 public class RDD_Basics {
-  public static void main(String[]args) {
-	  SparkConf conf = new SparkConf().setAppName("RDD_Basics").setMaster("local");
-	  JavaSparkContext context = new JavaSparkContext(conf);
-	  JavaRDD<String> rdd = context.textFile("/home/abhilash/Downloads/ml-1m/movies.dat");
-	  JavaRDD<String> rdd1 = rdd.filter(a -> a.contains("Comedy"));
-	  JavaRDD<Integer> rdd3 = rdd1.map(new MyFunctionClass());
-//	  JavaRDD<String> rdd2 = rdd1.flatMap(s -> Arrays.asList(s.split("::")).iterator());
-//	  JavaPairRDD<Integer, String> rdd2 = rdd1.mapToPair(s -> new Tuple2(rdd1[0],));
-//	  System.out.println("Total no of records in movies table:"+rdd.count());
-	  rdd3.saveAsTextFile("/home/abhilash/Desktop/rddfile3");
-//	  System.out.println(rdd2.collect());
-  }
+   public static void main(String[]args) {
+	   SparkConf conf = new SparkConf().setAppName("RDD_Basics").setMaster("local");
+	   JavaSparkContext context = new JavaSparkContext(conf);
+//	   String[]str = {"hello","abhilash","hello","hi","hello"};
+	   JavaRDD<String> rdd = context.textFile("/home/abhilash/Documents/files/file");
+//	   JavaRDD<String>
+//	   JavaRDD<String> rdd = context.parallelize(Arrays.asList(str));
+//	   JavaPairRDD<String,Integer> rdd1 = rdd.mapToPair(word -> new Tuple2<String,Integer>(word,1)).reduceByKey((a,b) -> a+b);
+//	   rdd1.saveAsTextFile("/home/abhilash/Documents/rddfile");
+	
+	   /*  for creating a user define function and applying certain transformation.
+	   JavaPairRDD<String,Character> rdd2 = rdd.mapToPair(word -> new Tuple2<String,Character>(word,User_define.first(word)));  
+	   rdd2.saveAsTextFile("/home/abhilash/Documents/rddfile");
+	   */
+	   
+   }
 }
